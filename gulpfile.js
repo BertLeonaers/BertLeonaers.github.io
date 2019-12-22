6b0a47d50c6536c9cf93b534facde59a2943fc08
Bert Leonaers <leonaerske@hotmail.com>
2019-12-22 18:38:49 +0100

    sass + browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,34 @@ const { src, dest, parallel, series, watch } = require('gulp');
 
 const gulpPug = require('gulp-pug');
 
+const browserSync = require('browser-sync').create();
+
+const sass        = require('gulp-sass');
+
 function pugToHTML(){
     return src("./pug/**/*.pug")
       .pipe(gulpPug({
-        pretty: true
+        pretty: true,
+        title: "Laurelle"
       }))
       .pipe(dest('./'))
   }
 
+function sassToCss(){
+    return src("./pug/sass/*.sass")
+      .pipe(sass())
+      .pipe(dest('css'))
+      .pipe(browserSync.stream())
+  }
+
 exports.default = function() {
+  sassToCss();
   pugToHTML()
-  watch('./pug/**/*.pug').on('change', pugToHTML)
+  browserSync.init({
+    server: {
+      baseDir: '.'
+    }
+  })
+  watch('./pug/**/*.pug').on('change', series(pugToHTML, browserSync.reload))
+  watch('./pug/sass/*.sass').on('change', sassToCss, browserSync.reload)
 };
