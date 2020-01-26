50cd2a80ba86246dced642b17c6c4b9c534c6cad
Bert Leonaers <leonaerske@hotmail.com>
2020-01-26 15:41:15 +0100

    PUG

--- gulpfile copy.js
+++ gulpfile copy.js
@@ -7,30 +7,24 @@ const browserSync = require('browser-sync').create();
 const sass        = require('gulp-sass');
 
 function pugToHTML(){
-  return src("./pug/**/*.pug")
-    .pipe(gulpPug({
-      pretty: true,
-      title: "Laurelle"
-    }))
-    .pipe(dest('./'))
-}
+    return src("./pug/**/*.pug")
+      .pipe(gulpPug({
+        pretty: true,
+        title: "Laurelle"
+      }))
+      .pipe(dest('./'))
+  }
 
 function sassToCss(){
-  return src("./pug/sass/*.sass")
-    .pipe(sass())
-    .pipe(dest('css'))
-    .pipe(browserSync.stream())
-}
-
-function js(){
-  return src("./pug/js/*.js")
-    .pipe(dest('js'));
-}
+    return src("./pug/sass/*.sass")
+      .pipe(sass())
+      .pipe(dest('css'))
+      .pipe(browserSync.stream())
+  }
 
 exports.default = function() {
   sassToCss();
-  pugToHTML();
-  js();
+  pugToHTML()
   browserSync.init({
     server: {
       baseDir: '.'
@@ -38,5 +32,4 @@ exports.default = function() {
   })
   watch('./pug/**/*.pug').on('change', series(pugToHTML, browserSync.reload))
   watch('./pug/sass/*.sass').on('change', sassToCss, browserSync.reload)
-  watch('./pug/js/*.js').on('change', js, browserSync.reload)
 };
---
+++ pug/js/main.js
@@ -0,0 +1,5 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+    //document.getElementsByTagName('nav')[0].classList.toggle('change');
+    document.getElementsByClassName('navigation')[0].classList.toggle('change');
+  }
