31cb3e0ab191a3a3a11bffd6b69354e052d2ed60
Bert Leonaers <leonaerske@hotmail.com>
2021-01-06 01:12:54 +0100

    Adapt gulp files generated + add sitemap

--- gulpfile.js
+++ gulpfile.js
@@ -10,25 +10,32 @@ const autoprefixer = require('gulp-autoprefixer');
 
 var del = require('del');
 
+var sitemap = require('gulp-sitemap');
+
+function defaultTask(cb) {
+  // place code for your default task here
+  cb();
+}
+
 function pugToHTML(){
-  return src("./pug/**/*.pug")
+  return src(["./pug/**/*.pug", "!./pug/template/*.pug", "!./pug/icons/*.pug"])
     .pipe(gulpPug({
       pretty: true,
       title: "Laurelle"
     }))
-    .pipe(dest('./generated/'))
+    .pipe(dest('./generated/'));
 }
 
-function sassToCss(){
+function sassToCss(cb){
   return src("./pug/sass/main.sass")
     .pipe(sass())
     .pipe(autoprefixer({
         cascade: false
       }))
     .pipe(dest('./generated/css'))
-    .pipe(browserSync.stream())
+    .pipe(browserSync.stream());
+    cb();
 }
-
 function js(){
   return src("./pug/js/*.js")
     .pipe(dest('./generated/js'));
@@ -39,23 +46,29 @@ function pictures(){
     .pipe(dest('./generated/pictures'));
 }
 
-function deleteGenerated(){
-  return del.sync("./generated");
+function generateSitemap(){
+  return src('generated/**/*.html', {
+    read: false
+})
+.pipe(sitemap({
+    siteUrl: 'bertleonaers.github.io'
+}))
+.pipe(dest('./generated'));
 }
 
-exports.default = function() {
-  //deleteGenerated();
-  sassToCss();
-  pugToHTML();
-  js();
-  pictures();
+function doBrowsersync(){
   browserSync.init({
     server: {
       baseDir: './generated'
     }
   })
+}
+
+function watchFiles(){
   watch('./pug/**/*.pug').on('change', series(pugToHTML, browserSync.reload))
   watch('./pug/sass/*.sass').on('change', sassToCss, browserSync.reload)
   watch('./pug/js/*.js').on('change', js, browserSync.reload)
   watch('./pug/pictures/**/*').on('change', pictures, browserSync.reload)
-};
+}
+
+exports.default = series(parallel(pugToHTML, sassToCss, js, pictures), generateSitemap, doBrowsersync, watchFiles)
